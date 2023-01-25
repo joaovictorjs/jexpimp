f46a4012607b3e050f99ec07936eec5662498118
joaovictorjs <skai7ami@gmail.com>
2023-01-25 09:38:48 -0300

    v0.0.1-dev

--- src/main/java/com/github/joaovictorjs/Jexpimp.java
+++ src/main/java/com/github/joaovictorjs/Jexpimp.java
@@ -5,9 +5,18 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class Jexpimp {
+    /**
+     * Returns an object from a JSON file.
+     *
+     * @param path the path where JSON file is located
+     * @param type the type of the object to be converted
+     * @return the return depends on the type you passed and the object that is expecting the return
+     */
     public static <T extends Object> T importJSON(String path, Class<T> type) throws NotJSONFileException, IOException {
         path = FilenameUtils.separatorsToSystem(path);
 
@@ -24,5 +33,54 @@ public class Jexpimp {
         fis.close();
 
         return new Gson().fromJson(raw, type);
+    }
+
+    /**
+     * exports a JSON file containing the data you passed
+     *
+     * @param path                  the path where the JSON file will be saved
+     * @param data                  the data that will be stored in the file
+     * @param excludeWhenDuplicated deletes the file if it already exists
+     * @return returns true if everything went well and false if something went wrong
+     */
+    public static <T extends Object> boolean exportJSON(String path, T data, boolean excludeWhenDuplicated) throws NotJSONFileException, IOException {
+        path = FilenameUtils.separatorsToSystem(path);
+
+        if (!path.endsWith(".json")) throw new NotJSONFileException(path);
+
+        String parent = new File(path).getParent();
+
+        if (parent == null || parent.equals("") || parent.equals(".") || parent.equals("." + File.separator)) {
+
+        } else {
+            if (!new File(parent).mkdirs()) {
+                return false;
+            }
+        }
+
+        File target = new File(path);
+
+        if (target.exists()) {
+            if (excludeWhenDuplicated) {
+                if (!target.delete()) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        if (!target.createNewFile()) {
+            return false;
+        }
+
+        String raw = new Gson().toJson(data);
+
+        FileOutputStream fos = new FileOutputStream(target);
+        fos.write(raw.getBytes());
+        fos.flush();
+        fos.close();
+
+        return target.exists() && target.length() == raw.getBytes().length;
     }
 }
--- src/test/java/JexpimpTests.java
+++ src/test/java/JexpimpTests.java
@@ -1,9 +1,13 @@
 import com.github.joaovictorjs.Jexpimp;
 import com.github.joaovictorjs.NotJSONFileException;
 import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.jupiter.api.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 class Mock {
     String name;
@@ -13,6 +17,7 @@ class Mock {
         this.name = "joaovictorjs";
         this.surname = "jexpimp";
     }
+
     Mock(String name, String surname) {
         this.name = name;
         this.surname = surname;
@@ -21,12 +26,85 @@ class Mock {
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JexpimpTests {
+    @AfterAll
+    public static void cleanup() {
+        new File(FilenameUtils.separatorsToSystem("./mock.json")).delete();
+    }
+
     @Test
-    @Order(9)
+    @Order(1)
+    public void exportJSONTest() {
+        try {
+            boolean res = Jexpimp.exportJSON("./mock.json", new Mock(), false);
+            Assertions.assertTrue(res);
+        } catch (NotJSONFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @Order(2)
+    public void exportNonJSONTest() {
+        Assertions.assertThrows(NotJSONFileException.class, () -> {
+            Jexpimp.exportJSON("./mock.ini", new Mock(), false);
+        });
+    }
+
+    @Test
+    @Order(3)
+    public void exportWhenDuplicatedTest() {
+        try {
+            boolean resFalse = Jexpimp.exportJSON("./mock.json", new Mock(), false);
+            boolean resTrue = Jexpimp.exportJSON("./mock.json", new Mock(), true);
+            Assertions.assertFalse(resFalse);
+            Assertions.assertTrue(resTrue);
+        } catch (NotJSONFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @Order(4)
+    public void exportNestedPathTest() {
+        File root = new File(FilenameUtils.separatorsToSystem("./home"));
+        String[] nested = {"a", "b", "c", "d", "e"};
+
+        for (String n : nested) {
+            root = new File(root, n);
+        }
+
+        root = new File(root, "mock.json");
+
+        try {
+            boolean res = Jexpimp.exportJSON(root.getPath(), new Mock(), false);
+            Assertions.assertTrue(res);
+            Assertions.assertTrue(root.exists());
+
+            root.delete();
+
+            File parent = new File(root.getParent());
+
+            for (int i = 0; i < 6; i++) {
+                parent.delete();
+                parent = new File(parent.getParent());
+            }
+        } catch (NotJSONFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @Order(5)
     public void importTest() {
         try {
             Mock expected = new Mock();
-            Mock res = Jexpimp.importJSON("mock.json", Mock.class);
+            Mock res = Jexpimp.importJSON("./mock.json", Mock.class);
             Assertions.assertTrue(expected.name.equals(res.name));
             Assertions.assertTrue(expected.surname.equals(res.surname));
         } catch (NotJSONFileException e) {
